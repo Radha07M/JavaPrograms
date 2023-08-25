7d7fe20e9fd55aea3cf206a341abe77bcc489352
Radha07M <radha07mane@gmail.com>
2023-08-25 19:27:24 +0530

    Commitedcode
    Commitedcode

---
+++ src/test/java/LeetCode/DateDiffCalculator.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+
+public class DateDiffCalculator {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Start Date");
+		String startDate = sc.next();
+		System.out.println(startDate);
+		String[] startArr = startDate.split("/");
+		int startYear = Integer.parseInt(startArr[2]);
+		int startMonth = Integer.parseInt(startArr[1]);
+		int startday = Integer.parseInt(startArr[0]);
+		
+		System.out.println("Enter End Date");
+		String endDate = sc.next();
+		System.out.println(endDate);
+		String[] endArr = endDate.split("/");
+		int endYear = Integer.parseInt(endArr[2]);
+		int endMonth = Integer.parseInt(endArr[1]);
+		int endDay = Integer.parseInt(endArr[0]);
+		
+		LocalDate firstDate = LocalDate.of(startYear, startMonth, startday);
+		LocalDate secondDate = LocalDate.of(endYear, endMonth, endDay);
+		
+		Period diff = Period.between(firstDate,secondDate);
+		
+		System.out.println("Difference is: Year= "+diff.getYears()+" ,Month= "+diff.getMonths()+" ,Days="+diff.getDays());
+	}
+
+}
