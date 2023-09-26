625d3f31900a8a6ec17fdc06e8c6e87d4153c1c6
Radha07M <radha07mane@gmail.com>
2023-09-26 20:40:43 +0530

    Added property file handling
    Added property file handling

--- src/test/java/FileManagement/PropertyFileHandling.java
+++ src/test/java/FileManagement/PropertyFileHandling.java
@@ -1,6 +1,11 @@
 package FileManagement;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Properties;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -18,8 +23,37 @@ public class PropertyFileHandling {
 		System.out.println("\n---------"+method.getName()+" Testcase completed-----------\n");
 	}
 	
-	@Test
+	@Test(priority = 0)
+	public void writePropertyFile() {
+		try {
+			Properties prop = new Properties();
+			prop.put("firstName","John");
+			prop.put("lastName","Smith");
+			
+			FileOutputStream fileOut = new FileOutputStream(new File(".//src//test//resources//TestData//propertyFile.properties"));
+			prop.store(fileOut, "Property file created and store the data");
+			System.out.println("Property file created and store the data");
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test(priority = 1)
 	public void readPropertyFile() {
-		
+		try {
+			FileInputStream fileIn = new FileInputStream(new File(".//src//test//resources//TestData//propertyFile.properties"));
+			Properties prop = new Properties();
+			prop.load(fileIn);
+			
+			String firstName = prop.getProperty("firstName");
+			System.out.println("First Name: "+firstName);
+			
+			String lastName = prop.getProperty("lastName");
+			System.out.println("Last Name: "+lastName);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
